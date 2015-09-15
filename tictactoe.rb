80736aa2c2f6ff3ed03b7d9a2b376dec5b427457
Aviv Levinski <pugsiman@gmail.com>
2015-09-15 22:27:09 +0300

    Added all A.I. components + optimization

--- tictactoe.rb
+++ tictactoe.rb
@@ -1,37 +1,37 @@
 #Adding a new method to the class String that allows to check if, for example, "2" is an integer "in disguise".
 class String
-	def is_integer?
-		self.to_i.to_s == self
-	end
+  def is_integer?
+    self.to_i.to_s == self
+  end
 end
 
 module TicTacToe
-	class Game
-		
-		def initialize
-			@board = (1..9).to_a
-			@running = true
-			@exit = false
-		end
-		
-		def display_board
-			n = 0
-			b = @board
-			
-			puts ""
-			puts " -----------"
-			3.times do
-				puts "  " + b[n].to_s + " | " + b[n+1].to_s + " | " + b[n+2].to_s
-				puts " -----------"
-				n += 3
-			end
-			puts ""
-		end
-		
-		def x_turn
-			display_board
-			puts "Choose a number (1-9) to place your mark on, Player 1."
-			position = gets.chomp
+  class Game
+
+    def initialize
+      @board = (1..9).to_a
+      @running = true
+      @exit = false
+    end
+
+    def display_board
+      n = 0
+      b = @board
+
+      puts ""
+      puts " -----------"
+      3.times do
+        puts "  " + b[n].to_s + " | " + b[n+1].to_s + " | " + b[n+2].to_s
+        puts " -----------"
+        n += 3
+      end
+      puts ""
+    end
+
+    def x_turn
+      display_board
+      puts "Choose a number (1-9) to place your mark on, Player 1."
+      position = gets.chomp
 			
 			#using personal created method to determine input
 			if position.is_integer?
@@ -84,8 +84,7 @@ module TicTacToe
 			elsif position.is_a?(String)
 				if position.downcase == "exit"
 					puts "Wow, rude. Bye."
-					#using return for a special case of both declaring @exit = true, and an early breakout.
-					return @exit = true
+					return @exit = true # return for a special case of both declaring and an early breakout.
 				end
 				puts "Position can only be a number, silly."
 				puts "Try again or type EXIT to, well, exit."
@@ -122,7 +121,7 @@ module TicTacToe
 		end
 		
 		def draw?
-			@board.all? { |all| all.is_a? String}
+			@board.all? {|all| all.is_a? String} #returns true if no one won by the end of the match.
 		end
 	
 		def result?
@@ -137,7 +136,7 @@ module TicTacToe
 			end
 		end
   
-		def game_progress
+		def playergame_progress
 			until !@running
 				x_turn
 				break if @exit
@@ -151,70 +150,107 @@ module TicTacToe
 		
 	end
 	
-	#AI component
-	class AI < Game
+	#AI components
 		def try_sides
 			if @board[1].is_a? Fixnum
-				@board[1] = "O"
-				return finish_turn = true
+				return @board[1] = "O"
 			elsif @board[3].is_a? Fixnum
-				@board[3] = "O"
-				return finish_turn = true
+				return @board[3] = "O"
 			elsif @board[5].is_a? Fixnum
-				@board[5] = "O"
-				return finish_turn = true
+				return @board[5] = "O"
 			elsif @board[7].is_a? Fixnum
-				@board[7] = "O"
-				return finish_turn = true
+				return @board[7] = "O"
 			end
 		end
 		
 		def try_corners
 			if @board[0].is_a? Fixnum
-				@board[0] = "O"
-				return finish_turn = true
+				return @board[0] = "O"
 			elsif @board[2].is_a? Fixnum
-				@board[2] = "O"
-				return finish_turn = true
+				return @board[2] = "O"
 			elsif @board[6].is_a? Fixnum
-				@board[6] = "O"
-				return finish_turn = true
+				return @board[6] = "O"
 			elsif @board[8].is_a? Fixnum
-				@board[8] = "O"
-				return finish_turn = true
+				return @board[8] = "O"
 			end
 		end
 		
-		def ai_evaluation
-			finish_turn = false
-			for i in (0..9)
-				origin = @board[i]
-				@board[i] = "O" if !@board[i] == "X"
-				if win_game?
-					return finish_turn = true
-				else
-					@board[i] = origin
-				end
-			end
-			
-			while !finish_turn
-				for i in 0..9 
-					
-				end
-			end
+		def ai_turn
+      #first check if possible to win before human player.
+      for i in (0...9)
+        origin = @board[i]
+        @board[i] = "O" if @board[i] != "X"
+        win_game? ? return : @board[i] = origin #using return for early breakout if win_game? is true.
+      end
+      
+      #if not possible to win before player, check if possible to block player from winning.
+      for i in (0...9)
+        origin = @board[i]
+        @board[i] = "X" if @board[i] != "O"
+        if win_game?
+          return @board[i] = "O" #if player can win that way, place it there before him.
+        else
+          @board[i] = origin
+        end
+      end
+
+      #if impossible to win nor block, default placement to center.
+      if !@board[4].is_a? String
+        return @board[4] = "O"
+      end
+      
+      #if default is occupied, choose between corners or sides randomly.
+      if @board[4].is_a? String
+        rand > 0.499 ? try_sides || try_corners : try_corners || try_sides
+      end
 		end
 		
-		def ai_pacing
-			finish_turn = false
-			
+		def aigame_progress
+      if rand > 0.3
+        until !@running
+          x_turn
+          break if @exit
+          result?
+          break if !@running
+          puts "Evil A.I. is scheming."
+          sleep(0.6)
+          puts "Evil A.I. is scheming.."
+          sleep(0.6)
+          puts "Evil A.I. is scheming..."
+          sleep(0.3)
+          ai_turn
+          result?
+        end
+      else
+        until !@running
+          puts "Evil A.I. is scheming."
+          sleep(0.6)
+          puts "Evil A.I. is scheming.."
+          sleep(0.6)
+          puts "Evil A.I. is scheming..."
+          sleep(0.3)
+          ai_turn
+          result?
+          break if !@running
+          x_turn
+          break if @exit
+          result?
+        end
+      end
 		end
-	end
 end
 
 def play
-	include TicTacToe
+  include TicTacToe
 	match = Game.new
-	match.game_progress
+  puts "Welcome to Tic Tac Toe, enter 1 to play against another player, or 2 to play against an evil A.I."
+  puts "Type EXIT anytime to quit"
+  choice = gets.chomp.to_i
+  case choice
+    when 1 then match.playergame_progress
+    when 2 then match.aigame_progress
+    else        puts "You silly, you."
+  end
 end
 
 play
