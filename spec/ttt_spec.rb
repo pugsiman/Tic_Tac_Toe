40cd07dc4d3428476d1c808c750725ed4f03fb31
Aviv Levinsky <pugsiman@gmail.com>
2015-10-24 03:21:43 +0300

    Slight optimization and basic spec tests

---
+++ spec/ttt_spec.rb
@@ -0,0 +1,60 @@
+require '../tictactoe.rb'
+
+# The game is initially not planned so well object oriented design-wise,
+# So the tests are limited to the basic methods
+
+describe Game do
+  let(:game) { Game.new }
+
+  describe '#start_screen' do
+    it 'displays introduction to the game' do
+      expect(game.start_screen)
+    end
+  end
+
+  describe '#display_board' do
+    it 'displays the board' do
+      expect(game.display_board)
+    end
+  end
+
+  describe '#win_game?' do
+    it 'returns true if game is won, and false if not' do
+      game.board = ['X', 'X', 'X', 4, 5, 6, 7, 8, 9]
+      expect(game.win_game?).to eq true
+      game.board = ['X', 2, 3, 4, 'X', 6, 7, 8, 'X']
+      expect(game.win_game?).to eq true
+      game.board = ['O', 2, 3, 4, 'O', 6, 7, 8, 'O']
+      expect(game.win_game?).to eq true
+      game.board = ['X', 'O', 'X', 4, 5, 6, 7, 8, 9]
+      expect(game.win_game?).to eq false
+    end
+  end
+
+  describe '#draw?' do
+    it 'returns true if game is finished in draw, and false if not' do
+      game.board = %w(X X O O X O O X O)
+      expect(game.draw?).to eq true
+      game.board = %w(X X O O X O O X O)
+      expect(game.draw?).to eq true
+      game.board = ['O', 2, 3, 4, 'X', 6, 7, 8, 'O']
+      expect(game.draw?).to eq false
+    end
+  end
+
+  describe '#check_win' do
+    it 'places mark on winning position if it will lead to win' do
+      game.board = ['X', 'O', 3, 4, 'O', 'X', 7, 8, 9]
+      expect(game.check_win)
+      expect(game.win_game?).to eq true
+    end
+  end
+
+  describe '#check_block' do
+    it 'places mark on blocking position if it will block player\'s win' do
+      game.board = ['X', 'X', 3, 4, 'O', 6, 7, 8, 9]
+      expect(game.check_block)
+      expect(game.win_game?).to eq false
+    end
+  end
+end
--- tictactoe.rb
+++ tictactoe.rb
@@ -1,39 +1,50 @@
-# Checks if, for example, "2" is an integer "in disguise".
-class String
-  def is_integer?
-    to_i.to_s == self
-  end
-end
-
 # Main TicTacToe game class
 class Game
+  attr_writer :board # for tests
+
   def initialize
     @board = (1..9).to_a
     @running = true
+    start_screen
   end
 
-  def display_board
-    puts "\n -----------"
-    @board.each_slice(3) do |row|
-      print '  '
-      puts row.join(' | ')
-      puts ' -----------'
+  def start_screen
+    puts 'Welcome to Tic Tac Toe.'
+    puts 'Enter 1 to play against another player, 2 to play against an evil AI.'
+    puts 'Type EXIT anytime to quit.'
+
+    choice = gets.chomp.to_i
+    case choice
+    when 1 then playergame_progress
+    when 2 then aigame_progress
+    else puts 'You silly, you.'
     end
-    puts
+  end
+
+  def playergame_progress
+    while @running
+      turn(:X)
+      result?
+      break unless @running
+      turn(:O)
+      result?
+    end
+  end
+
+  def aigame_progress
+    rand > 0.3 ? player_first : ai_first
   end
 
   def turn(chosen_player)
     display_board
     puts "Choose a number (1-9) to place your mark on, Player #{chosen_player}."
     position = gets.chomp
-
-    # using personal created method to determine input
-    position = position.to_i if position.is_integer?
+    position = position.to_i unless position.to_i == 0
 
     if @board.include?(position)
       @board.map! do |num|
         if num == position
-          determine_player(chosen_player)
+          chosen_player.to_s
         else
           num
         end
@@ -50,32 +61,14 @@ class Game
     end
   end
 
-  def exit_game
-    puts 'Wow, rude. Bye.'
-    exit
-  end
-
-  def determine_player(player)
-    player.to_s
-  end
-
-  def win_game?
-    sequences = [[0, 1, 2], [3, 4, 5], [6, 7, 8],
-                 [0, 3, 6], [1, 4, 7], [2, 5, 8],
-                 [0, 4, 8], [2, 4, 6]]
-
-    sequences.each do |sequence|
-      if sequence.all? { |a| @board[a] == 'X' }
-        return true
-      elsif sequence.all? { |a| @board[a] == 'O' }
-        return true
-      end
+  def display_board
+    puts "\n -----------"
+    @board.each_slice(3) do |row|
+      print '  '
+      puts row.join(' | ')
+      puts ' -----------'
     end
-    false
-  end
-
-  def draw?
-    @board.all? { |all| all.is_a? String } # returns true if no one has won.
+    puts
   end
 
   def result?
@@ -90,101 +83,122 @@ class Game
     end
   end
 
-  def playergame_progress
-    while @running
-      turn(:X)
-      result?
-      break if !@running
-      turn(:O)
-      result?
+  def win_game?
+    sequences = [[0, 1, 2], [3, 4, 5], [6, 7, 8],
+                 [0, 3, 6], [1, 4, 7], [2, 5, 8],
+                 [0, 4, 8], [2, 4, 6]]
+
+    sequences.each do |seq|
+      if seq.all? { |a| @board[a] == 'X' } || seq.all? { |a| @board[a] == 'O' }
+        return true
+      end
     end
+    false
+  end
+
+  def draw?
+    @board.all? { |all| all.is_a? String } # returns true if no one has won.
+  end
+
+  def exit_game
+    puts 'Wow, rude. Bye.'
+    exit
   end
 
   # AI components
-  def try_sides
+  def check_win
+    # first check if possible to win before human player.
+    @finished = false
+    0.upto(8) do |i|
+      origin = @board[i]
+      @board[i] = 'O' unless @board[i] == 'X'
+      if win_game?
+        return @finished = true # early breakout if won game.
+      else
+        @board[i] = origin
+      end
+    end
+  end
+
+  def check_block
+    # if impossible to win before player,
+    # check if possible to block player from winning.
+    @finished = false
+    0.upto(8) do |i|
+      origin = @board[i]
+      @board[i] = 'X' unless @board[i] == 'O'
+      if win_game?
+        return [@finished = true, @board[i] = 'O']
+        # if player can win that way, place it there.
+      else
+        @board[i] = origin
+      end
+    end
+  end
+
+  def possible_sides
     [1, 3, 5, 7].each do |idx|
       return @board[idx] = 'O' if @board[idx].is_a? Fixnum
     end
   end
 
-  def try_corners
+  def possible_corners
     [0, 2, 6, 8].each do |idx|
       return @board[idx] = 'O' if @board[idx].is_a? Fixnum
     end
   end
 
-  def ai_turn
-    # first check if possible to win before human player.
-    0.upto(8) do |i|
-      origin = @board[i]
-      @board[i] = 'O' unless @board[i] == 'X'
-      win_game? ? return : @board[i] = origin # early breakout if won game.
-    end
-
-    # if impossible to win before player, check if possible to block player from winning.
-    0.upto(8) do |i|
-      origin = @board[i]
-      @board[i] = 'X' unless @board[i] == 'O'
-      if win_game?
-        return @board[i] = 'O' # if player can win that way, place it there.
-      else
-        @board[i] = origin
-      end
-    end
-
+  def check_defaults
     # if impossible to win nor block, default placement to center.
     # if occupied, choose randomly between corners or sides.
     if @board[4].is_a? Fixnum
       return @board[4] = 'O'
     else
-      rand > 0.499 ? try_sides || try_corners : try_corners || try_sides
+      if rand > 0.499
+        possible_sides
+      else
+        possible_corners
+      end
     end
   end
 
-  def thinking_simulation
+  def loading_simulation
     str = "\rEvil AI is scheming"
     5.times do
       print str += '.'
-      sleep(0.3)
+      sleep(0.25)
     end
   end
 
-  def aigame_progress
-    if rand > 0.3
-      while @running
-        turn(:X)
-        result?
-        break if !@running
-        thinking_simulation
-        ai_turn
-        result?
-      end
-    else
-      while @running
-        thinking_simulation
-        ai_turn
-        result?
-        break if !@running
-        turn(:X)
-        result?
-      end
+  def ai_turn
+    check_win
+    return if @finished
+    check_block
+    return if @finished
+    check_defaults
+  end
+
+  def player_first
+    while @running
+      turn(:X)
+      result?
+      break unless @running
+      loading_simulation
+      ai_turn
+      result?
+    end
+  end
+
+  def ai_first
+    while @running
+      loading_simulation
+      ai_turn
+      result?
+      break unless @running
+      turn(:X)
+      result?
     end
   end
 end
 
-def play
-  match = Game.new
-
-  puts 'Welcome to Tic Tac Toe.'
-  puts 'Enter 1 to play against another player, 2 to play against an evil AI.'
-  puts 'Type EXIT anytime to quit.'
-
-  choice = gets.chomp.to_i
-  case choice
-  when 1 then match.playergame_progress
-  when 2 then match.aigame_progress
-  else        puts 'You silly, you.'
-  end
-end
-
-play
+Game.new
